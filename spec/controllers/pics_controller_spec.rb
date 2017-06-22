7855875b3f381b3c40e95d7c9fa38be77a0be9ff
MelsenC <melsen.carlsburg@gmail.com>
2017-06-22 21:58:20 +0000

    started writing tests for the edit pic form page

--- spec/controllers/pics_controller_spec.rb
+++ spec/controllers/pics_controller_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe PicsController, type: :controller do
 
+  describe "pics#edit action" do
+    it "should successfully show the edit form if the pic is found" do
+
+    end
+
+    it "should return a 404 error if the pic is not found" do
+
+    end
+
+  end
+
   describe "pics#show action" do
     it "should successfully show the page if the pic is found" do
       pic = FactoryGirl.create(:pic)
