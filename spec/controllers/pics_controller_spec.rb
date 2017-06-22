b42c234e5b5c73d0c8e74aa2da28e2c02804f0db
MelsenC <melsen.carlsburg@gmail.com>
2017-06-22 19:53:59 +0000

    installed factory_girl_rails gem for storing reusable sample data for testing

--- spec/controllers/pics_controller_spec.rb
+++ spec/controllers/pics_controller_spec.rb
@@ -16,11 +16,7 @@ RSpec.describe PicsController, type: :controller do
     end
 
     it "should successfully show the new form" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       get :new
@@ -36,11 +32,7 @@ RSpec.describe PicsController, type: :controller do
     end
 
     it "should successfully create a new pic in the database" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, params: { pic: { message: 'Hello!' } }
@@ -52,11 +44,7 @@ RSpec.describe PicsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                  'fakeuser@gmail.com',
-        password:               'secretPassword',
-        password_confirmation:  'secretPassword'
-      )
+      user = FactoryGirl.create(:user)
       sign_in user
 
       post :create, params: { pic: { message: '' } }
